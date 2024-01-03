09dd688de0ef9fd592c88bc955abc82c157c3cea
FatemehJD <135868927+FatemehJD@users.noreply.github.com>
2024-01-03 17:12:18 -0600

    Update f61p.rs

--- src/field/f61p.rs
+++ src/field/f61p.rs
@@ -5,6 +5,7 @@ use generic_array::GenericArray;
 use rand_core::RngCore;
 use std::ops::{AddAssign, MulAssign, SubAssign};
 use subtle::{Choice, ConditionallySelectable, ConstantTimeEq};
+use std::convert::TryFrom;
 
 /// A finite field over the Mersenne Prime 2^61 - 1
 #[derive(Clone, Copy, Eq, Debug, Hash)]
